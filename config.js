faa73218d40b1e56dcb9ff9fcdf2481f053de27e
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-27 14:02:32 +0200

    removed nodemon

---
+++ config.js
@@ -0,0 +1,18 @@
+const config = {
+    'secretKey': '12345-67890-09876-54321',
+
+    'mongoUrl': 'mongodb://localhost:27017/',
+
+    'mongoAtlas': 'mongodb+srv://paul:GnWYs1E27E4pRSEg@codecamp.pnh7v.mongodb.net/codeCamp?retryWrites=true&w=majority',
+    'username': '',
+    'password': '',
+    'cluster': '',
+    'dbname': '',
+
+    'ACCESS_TOKEN_SECRET': '039a33d9ffe3e0375631d146e2948c1d62faffeb3c1ad3dbcdf945c9e409656d8db4fb05be3998884b7f7411a732bf7e11029fc4858ddc7ade83ec0c41dae124',
+
+    'REFRESH_TOKEN_SECRET': '262d22ca31462ae3c4a32b3de1ee24f40152ba0ea285052bf9caf7a68316b4e7f375a68fbef34a08812684fd78848ed425528d502c7a0012bf589c7ee7751178'
+
+}
+
+export default config;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var indexRouter = Router();
 /* GET home page. */
 //server the homepage
 indexRouter.get('/', (req, res, next) => {
+    res.status(200);
     res.send('Welcome to the Homepage');
 });
 
